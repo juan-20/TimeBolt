5738e6f6c80fb2357fca95226a6f98e3fcd271a7
juan-20 <juanalvesandrade@gmail.com>
2024-02-09 23:13:16 -0300

    removing style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,49 +6,7 @@ export const HomeContainer = styled.main`
     align-items: center;
     justify-content: center;
     gap: 1rem;
-    
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 1rem;
-    
-        label {
-        font-size: 1.5rem;
-        }
-    
-        input {
-        width: 100%;
-        padding: 1rem;
-        border: 0;
-        border-radius: 0.5rem;
-        background: ${(props) => props.theme["gray-800"]};
-        color: ${(props) => props.theme["gray-300"]};
-        }
-    
-        span {
-        font-size: 1.5rem;
-        }
-    
-        div {
-        display: flex;
-        gap: 0.5rem;
-        font-size: 2rem;
-        }
-    
-        button {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        padding: 1rem 2rem;
-        border: 0;
-        border-radius: 0.5rem;
-        background: ${(props) => props.theme["green-500"]};
-        color: ${(props) => props.theme["gray-900"]};
-        font-size: 1.5rem;
-        cursor: pointer;
-        }
-    }
+
     `;
 
 export const FormContainer = styled.form`
